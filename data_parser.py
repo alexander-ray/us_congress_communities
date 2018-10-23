26df60dcc8ab0cd2b3e07a29846f81ff4b36ca1a
alexander-ray <1alexray@gmail.com>
2018-10-22 23:19:06 -0600

    Added legislator lookup code

--- data_parser.py
+++ data_parser.py
@@ -15,20 +15,55 @@ def generate_graph(congress, origin):
 
     G = nx.DiGraph()
 
+    with open('./legislators/bioguide_lookup', 'r') as f:
+        bioguide_lookup = json.load(f)
+
+    def get_bioguide(d):
+        if 'bioguide_id' in d:
+            return d['bioguide_id']
+        return bioguide_lookup[d['thomas_id']]
+
     # Assumes dir structure of ./data/congress_num/bills/origin_indicator/*/data.json
     dir_path = './data/' + str(congress) + '/bills/' + origin + '/*/data.json'
     data_files = glob.glob(dir_path)
     for file in data_files:
         with open(file, 'r') as f:
             data = json.load(f)
-            sponsor_id = data['sponsor']['bioguide_id']
+            sponsor_id = get_bioguide(data['sponsor'])
             G.add_node(sponsor_id)
             G.nodes[sponsor_id]['name'] = data['sponsor']['name']
             G.nodes[sponsor_id]['state'] = data['sponsor']['state']
 
             for cosponsor in data['cosponsors']:
-                G.add_edge(cosponsor['bioguide_id'], sponsor_id)
+                G.add_edge(get_bioguide(cosponsor), sponsor_id)
 
     return G
 
-G = generate_graph(15, 's')
+
+def generate_bioguide_lookup():
+    """
+    Generate lookup tables for bioguide and thomas ids
+    """
+    thomas_lookup = {}
+    bioguide_lookup = {}
+
+    def load_data(f):
+        data = json.load(f)
+        for entry in data:
+            if 'bioguide' in entry['id'] and 'thomas' in entry['id']:
+                thomas_lookup[entry['id']['bioguide']] = entry['id']['thomas']
+                bioguide_lookup[entry['id']['thomas']] = entry['id']['bioguide']
+
+    with open('./legislators/legislators-current.json', 'r') as f:
+        load_data(f)
+    with open('./legislators/legislators-historical.json', 'r') as f:
+        load_data(f)
+
+    with open('./legislators/thomas_lookup', 'w') as fout:
+        fout.write(json.dumps(thomas_lookup))
+    with open('./legislators/bioguide_lookup', 'w') as fout:
+        fout.write(json.dumps(bioguide_lookup))
+
+
+generate_bioguide_lookup()
+G = generate_graph(93, 's')
