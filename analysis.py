4b859456be57d55b44a666a15d60c1ce3f5a1178
alexander-ray <1alexray@gmail.com>
2018-12-04 15:18:36 -0700

    Removed test

--- analysis.py
+++ analysis.py
@@ -4,11 +4,12 @@ import networkx as nx
 import csv
 import igraph
 from NMI import calc_nmi
-
+import math
 
 def update_nodes_to_numeric(G):
     """
     Converts networkx nodes to numeric, setting 'bioguide' to be old node label
+
     :param G: nx.Graph
     :return: nx.Graph
     """
@@ -18,6 +19,7 @@ def update_nodes_to_numeric(G):
 def calculate_modularity(G, degrees, s):
     """
     Helper function to calculate weighted modularity. Same result as iGraph, but slower
+
     :param G: np array adjacency matrix
     :param degrees: ordered list of degrees
     :param s: ordered list of group labels
@@ -36,6 +38,7 @@ def calculate_modularity(G, degrees, s):
 def generate_membership_list(nodes):
     """
     Helper function to generate ordered list of group labels from Networkx nodeset
+
     :param nodes: Networkx nodes
     :return: ordered list of group labels
     """
@@ -50,53 +53,83 @@ def generate_membership_list(nodes):
     return s
 
 
+def apply_threshold(G, num_legislators, threshold):
+    """
+    Apply threshold to bills in bipartite network. Keep bills with sub-threshold sponsorship
+
+    :param G: Bipartite nx.Graph
+    :param num_legislators: Number of legislators in this chamber & congress
+    :param threshold: Threshold to apply
+    :return: nx.Graph
+    """
+    threshold = math.ceil(threshold * num_legislators)
+    nodes_to_remove = []
+    s = 0
+    c = 0
+    for node in G.nodes:
+        c += 1
+        s += G.degree[node]
+        if G.nodes[node]['type'] == 'bill' and G.degree[node] > threshold:
+            nodes_to_remove.append(node)
+    G.remove_nodes_from(nodes_to_remove)
+    return G
+
+
 house_names = ['hr', 'hconres', 'hjres', 'hres', 'hamendments']
-senate_names = ['s', 'sconres', 'sjres', 'sres', 'samendments']
+#senate_names = ['s', 'sconres', 'sjres', 'sres', 'samendments']
+senate_names = ['s']
 chamber = 'senate'
+#thresholds = [0.6, 0.7, 0.8, 0.9, 1.0]
+thresholds = [0.6, 0.7, 0.8, 0.9, 1.0]
 
 mod_arr = []
 congresses = list(range(96, 116))
-for congress in congresses:
-    # Check against "truth" in https://www.sciencedirect.com/science/article/pii/S037843710701206X?via%3Dihub
-    '''
-    G = generate_digraph('/Users/alexray/Documents/data/', senate, senate_names)
-    print(len(G))
-    avg_degree = np.mean([deg[1] for deg in G.in_degree])
-    print(avg_degree)
-    print(nx.average_shortest_path_length(G))
-    '''
-    if chamber == 'house':
-        G_bipartite = generate_bipartite_graph('/Users/alexray/Documents/data/', congress, house_names)
-    else:
-        G_bipartite = generate_bipartite_graph('/Users/alexray/Documents/data/', congress, senate_names)
-    G_bipartite= update_nodes_to_numeric(G_bipartite)
-    legislators = [node for node in G_bipartite.nodes if G_bipartite.nodes[node]['type'] == 'legislator']
-    G_proj = nx.bipartite.weighted_projected_graph(G_bipartite, legislators)
-    # Uncomment to include self-loops
-    #for node in G_proj.nodes:
-    #    G_proj.add_edge(node, node, weight=G_bipartite.degree[node])
-    A = nx.to_numpy_array(G_proj, nodelist=G_proj.nodes)
+for threshold in thresholds:
+    print(threshold)
+    for congress in congresses:
+        # Check against "truth" in https://www.sciencedirect.com/science/article/pii/S037843710701206X?via%3Dihub
+        '''
+        G = generate_digraph('/Users/alexray/Documents/data/', senate, senate_names)
+        print(len(G))
+        avg_degree = np.mean([deg[1] for deg in G.in_degree])
+        print(avg_degree)
+        print(nx.average_shortest_path_length(G))
+        '''
+        if chamber == 'house':
+            G_bipartite = generate_bipartite_graph('/Users/alexray/Documents/data/', congress, house_names)
+        else:
+            G_bipartite = generate_bipartite_graph('/Users/alexray/Documents/data/', congress, senate_names)
+        G_bipartite= update_nodes_to_numeric(G_bipartite)
+        legislators = [node for node in G_bipartite.nodes if G_bipartite.nodes[node]['type'] == 'legislator']
+        if threshold is not None:
+            G_bipartite = apply_threshold(G_bipartite, len(legislators), threshold)
+        G_proj = nx.bipartite.weighted_projected_graph(G_bipartite, legislators)
+        # Uncomment to include self-loops
+        #for node in G_proj.nodes:
+        #    G_proj.add_edge(node, node, weight=G_bipartite.degree[node])
 
-    s = generate_membership_list(G_proj.nodes)
+        A = nx.to_numpy_array(G_proj, nodelist=G_proj.nodes)
 
-    # Networkx to igraph: https://stackoverflow.com/questions/29655111/
-    g = igraph.Graph.Adjacency((A > 0).tolist())
-    g.es['weight'] = A[A.nonzero()]
-    g.vs['label'] = list(G_proj.nodes)
-    mod = g.modularity(membership=s, weights='weight')
-    eig_result = g.community_leading_eigenvector(clusters=None, weights='weight')
-    max_mod = eig_result.modularity
+        s = generate_membership_list(G_proj.nodes)
 
-    eig_s = []
-    for i in range(len(G_proj)):
-        j = 0
-        for group in eig_result:
-            if i in group:
-                eig_s.append(j)
-                break
-            j += 1
+        # Networkx to igraph: https://stackoverflow.com/questions/29655111/
+        g = igraph.Graph.Adjacency((A > 0).tolist())
+        g.es['weight'] = A[A.nonzero()]
+        g.vs['label'] = list(G_proj.nodes)
+        mod = g.modularity(membership=s, weights='weight')
+        eig_result = g.community_leading_eigenvector(clusters=None, weights='weight')
+        max_mod = eig_result.modularity
 
-    # Uncomment to write results to csv for later use
-    with open('senate_nmi_no_self.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow([congress, calc_nmi(s, eig_s)])
+        eig_s = []
+        for i in range(len(G_proj)):
+            j = 0
+            for group in eig_result:
+                if i in group:
+                    eig_s.append(j)
+                    break
+                j += 1
+
+        # Uncomment to write results to csv for later use
+        with open('./data/senate_thresholds_no_self_bills.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([threshold, congress, mod, max_mod])
--- plotting.py
+++ plotting.py
@@ -1,15 +1,28 @@
 import matplotlib.pyplot as plt
 import plotting_setup
+import numpy as np
 
-chamber = 'house'
-xlabel = 'House'
-ylabel = 'NMI'
-title = ''
+chamber = 'senate'
+xlabel = 'Senate'
+ylabel = 'Party Modularity'
+title = 'Party modularity across different thresholds'
 
 
 congress_mask = set(range(96, 116))
 data = []
-with open('./data/' + chamber + '_nmi_no_self.csv', 'r') as f:
+thresholds = {0.6: [], 0.7: [], 0.8: [], 0.9: [], 1.0: []}
+congress = list(range(96, 116))
+with open('./data/' + chamber + '_thresholds_no_self_bills.csv', 'r') as f:
+    for line in f:
+        line = line.strip().split(',')
+        thres = float(line[0]); mod = float(line[2]); mod_max = float(line[3])
+        thresholds[thres].append((mod, mod_max))
+
+for thres in thresholds.keys():
+    plt.plot(congress, [t[0] for t in thresholds[thres]], label=str(thres))
+
+'''
+with open('./data/' + chamber + '_senate_thresholds_no_self.csv', 'r') as f:
     for line in f:
         line = line.strip().split(',')
         #data.append((int(line[0]), float(line[1]), float(line[2])))
@@ -19,8 +32,12 @@ data = [d for d in data if d[0] in congress_mask]
 congress = [d[0] for d in data]
 nmi = [d[1] for d in data]
 #max_mod = [d[2] for d in data]
+poly = np.polyfit(congress, nmi, 1)
+plt.plot(congress, np.poly1d(poly)(congress))
 plt.scatter(congress, nmi)
 #plt.plot(congress, max_mod, linestyle='--', label='Max Q')
+'''
+
 ax = plt.gca()
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
