ab1ffa7c8d24df210a26cd8d644d0a36a9cd39c1
Aaron Aaeng <aaaa9270@colorado.edu>
2018-12-08 23:13:53 -0700

    not as much overlap as expected

--- bill_analysis.py
+++ bill_analysis.py
@@ -38,36 +38,42 @@ def perform_projection(G_bipartite, projected_node_type, threshold=None):
 
     return nx.bipartite.weighted_projected_graph(G_bipartite, legislators)
 
-def analyze_graph(G):
+def analyze_graph(G_bipartite):
     subject_modularities = {}
-    node_types = generate_attribute_dict(G, 'type')
+    bill_types = generate_attribute_dict(G_bipartite, 'subject')
+    node_types = generate_attribute_dict(G_bipartite, 'type')
     legislators = node_types['legislator']
-    bill_types = generate_attribute_dict(G, 'subject')
+
     for subject, valid_bills in bill_types.items():
-        filtered_graph = G.subgraph(legislators + valid_bills)
-        mod, _ = calculate_weighted_modularity(filtered_graph)
+        G_filtered = G_bipartite.subgraph(legislators + valid_bills)
+        G_proj = perform_projection(G_filtered, 'legislator')
+        mod, _ = calculate_weighted_modularity(G_proj)
         subject_modularities[subject] = mod
     
     return subject_modularities
 
-def all_analysis(congresses, file_names):
+def chamber_analysis(congresses, file_names):
     df = pd.DataFrame(columns=['CONGRESS'])
 
     for congress in congresses:
         G_bipartite = generate_bipartite_graph('/home/eitri/git/us_congress_communities/data/', congress, file_names)
         congress_data = analyze_graph(G_bipartite)
         congress_data['CONGRESS'] = congress
-        df = pd.concat([df, congress_data], axis=0, ignore_index=True)
+        df_congress_data = pd.DataFrame(congress_data, index=[0])
+        df = pd.concat([df, df_congress_data], axis=0, sort=True)
     
     return df
 
 
-def analysis_test():
+def congress_analysis():
     house_names = ['hr']
-    congresses = [97]
+    senate_names = ['s']
+    congresses = list(range(96, 116))
 
-    df = all_analysis(congresses, house_names)
+    # df = chamber_analysis(congresses, house_names)
+    # df.to_csv('/home/eitri/Documents/house_output.csv', encoding='utf-8')
 
-    df.to_csv('/home/eitri/Documents/test_output.csv', sep='\t', encoding='utf-8')
+    df = chamber_analysis(congresses, senate_names)
+    df.to_csv('/home/eitri/Documents/senate_output.csv', encoding='utf-8')
 
-analysis_test()
+congress_analysis()
