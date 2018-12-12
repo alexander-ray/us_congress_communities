72eb91b1fc0d1e5868fc3ad29109d66a3b8f8f3d
crosswise1 <dacr4402@colorado.edu>
2018-12-12 16:09:41 -0700

    trying to commit

---
+++ bills_passing_chamber.py
@@ -0,0 +1,12 @@
+# from data_parser import generate_graph, generate_digraph, generate_bipartite_graph
+# import numpy as np
+# import networkx as nx
+# import csv
+# import igraph
+# from NMI import calc_nmi
+# import math
+
+def main():
+    print ('hello world')
+
+print ('yo does this print dog')
